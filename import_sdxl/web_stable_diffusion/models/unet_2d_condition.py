de7e0dd6bfd958b374e332ccc27a34a971684307
guoyaol <guoyaoli00@gmail.com>
2023-07-19 01:22:02 -0400

    modify model

--- import_sdxl/web_stable_diffusion/models/unet_2d_condition.py
+++ import_sdxl/web_stable_diffusion/models/unet_2d_condition.py
@@ -605,134 +605,134 @@ class TVMUNet2DConditionModel(nn.Module):
             block_out_channels[0], out_channels, kernel_size=conv_out_kernel, padding=conv_out_padding
         )
 
-    @property
-    def attn_processors(self) -> Dict[str, AttentionProcessor]:
-        r"""
-        Returns:
-            `dict` of attention processors: A dictionary containing all attention processors used in the model with
-            indexed by its weight name.
-        """
-        # set recursively
-        processors = {}
+    # @property
+    # def attn_processors(self) -> Dict[str, AttentionProcessor]:
+    #     r"""
+    #     Returns:
+    #         `dict` of attention processors: A dictionary containing all attention processors used in the model with
+    #         indexed by its weight name.
+    #     """
+    #     # set recursively
+    #     processors = {}
 
-        def fn_recursive_add_processors(name: str, module: torch.nn.Module, processors: Dict[str, AttentionProcessor]):
-            if hasattr(module, "set_processor"):
-                processors[f"{name}.processor"] = module.processor
+    #     def fn_recursive_add_processors(name: str, module: torch.nn.Module, processors: Dict[str, AttentionProcessor]):
+    #         if hasattr(module, "set_processor"):
+    #             processors[f"{name}.processor"] = module.processor
 
-            for sub_name, child in module.named_children():
-                fn_recursive_add_processors(f"{name}.{sub_name}", child, processors)
+    #         for sub_name, child in module.named_children():
+    #             fn_recursive_add_processors(f"{name}.{sub_name}", child, processors)
 
-            return processors
+    #         return processors
 
-        for name, module in self.named_children():
-            fn_recursive_add_processors(name, module, processors)
+    #     for name, module in self.named_children():
+    #         fn_recursive_add_processors(name, module, processors)
 
-        return processors
+    #     return processors
 
-    def set_attn_processor(self, processor: Union[AttentionProcessor, Dict[str, AttentionProcessor]]):
-        r"""
-        Sets the attention processor to use to compute attention.
+    # def set_attn_processor(self, processor: Union[AttentionProcessor, Dict[str, AttentionProcessor]]):
+    #     r"""
+    #     Sets the attention processor to use to compute attention.
 
-        Parameters:
-            processor (`dict` of `AttentionProcessor` or only `AttentionProcessor`):
-                The instantiated processor class or a dictionary of processor classes that will be set as the processor
-                for **all** `Attention` layers.
+    #     Parameters:
+    #         processor (`dict` of `AttentionProcessor` or only `AttentionProcessor`):
+    #             The instantiated processor class or a dictionary of processor classes that will be set as the processor
+    #             for **all** `Attention` layers.
 
-                If `processor` is a dict, the key needs to define the path to the corresponding cross attention
-                processor. This is strongly recommended when setting trainable attention processors.
+    #             If `processor` is a dict, the key needs to define the path to the corresponding cross attention
+    #             processor. This is strongly recommended when setting trainable attention processors.
 
-        """
-        count = len(self.attn_processors.keys())
+    #     """
+    #     count = len(self.attn_processors.keys())
 
-        if isinstance(processor, dict) and len(processor) != count:
-            raise ValueError(
-                f"A dict of processors was passed, but the number of processors {len(processor)} does not match the"
-                f" number of attention layers: {count}. Please make sure to pass {count} processor classes."
-            )
+    #     if isinstance(processor, dict) and len(processor) != count:
+    #         raise ValueError(
+    #             f"A dict of processors was passed, but the number of processors {len(processor)} does not match the"
+    #             f" number of attention layers: {count}. Please make sure to pass {count} processor classes."
+    #         )
 
-        def fn_recursive_attn_processor(name: str, module: torch.nn.Module, processor):
-            if hasattr(module, "set_processor"):
-                if not isinstance(processor, dict):
-                    module.set_processor(processor)
-                else:
-                    module.set_processor(processor.pop(f"{name}.processor"))
+    #     def fn_recursive_attn_processor(name: str, module: torch.nn.Module, processor):
+    #         if hasattr(module, "set_processor"):
+    #             if not isinstance(processor, dict):
+    #                 module.set_processor(processor)
+    #             else:
+    #                 module.set_processor(processor.pop(f"{name}.processor"))
 
-            for sub_name, child in module.named_children():
-                fn_recursive_attn_processor(f"{name}.{sub_name}", child, processor)
+    #         for sub_name, child in module.named_children():
+    #             fn_recursive_attn_processor(f"{name}.{sub_name}", child, processor)
 
-        for name, module in self.named_children():
-            fn_recursive_attn_processor(name, module, processor)
+    #     for name, module in self.named_children():
+    #         fn_recursive_attn_processor(name, module, processor)
 
-    def set_default_attn_processor(self):
-        """
-        Disables custom attention processors and sets the default attention implementation.
-        """
-        self.set_attn_processor(AttnProcessor())
+    # def set_default_attn_processor(self):
+    #     """
+    #     Disables custom attention processors and sets the default attention implementation.
+    #     """
+    #     self.set_attn_processor(AttnProcessor())
 
-    def set_attention_slice(self, slice_size):
-        r"""
-        Enable sliced attention computation.
+    # def set_attention_slice(self, slice_size):
+    #     r"""
+    #     Enable sliced attention computation.
 
-        When this option is enabled, the attention module splits the input tensor in slices to compute attention in
-        several steps. This is useful for saving some memory in exchange for a small decrease in speed.
+    #     When this option is enabled, the attention module splits the input tensor in slices to compute attention in
+    #     several steps. This is useful for saving some memory in exchange for a small decrease in speed.
 
-        Args:
-            slice_size (`str` or `int` or `list(int)`, *optional*, defaults to `"auto"`):
-                When `"auto"`, input to the attention heads is halved, so attention is computed in two steps. If
-                `"max"`, maximum amount of memory is saved by running only one slice at a time. If a number is
-                provided, uses as many slices as `attention_head_dim // slice_size`. In this case, `attention_head_dim`
-                must be a multiple of `slice_size`.
-        """
-        sliceable_head_dims = []
+    #     Args:
+    #         slice_size (`str` or `int` or `list(int)`, *optional*, defaults to `"auto"`):
+    #             When `"auto"`, input to the attention heads is halved, so attention is computed in two steps. If
+    #             `"max"`, maximum amount of memory is saved by running only one slice at a time. If a number is
+    #             provided, uses as many slices as `attention_head_dim // slice_size`. In this case, `attention_head_dim`
+    #             must be a multiple of `slice_size`.
+    #     """
+    #     sliceable_head_dims = []
 
-        def fn_recursive_retrieve_sliceable_dims(module: torch.nn.Module):
-            if hasattr(module, "set_attention_slice"):
-                sliceable_head_dims.append(module.sliceable_head_dim)
+    #     def fn_recursive_retrieve_sliceable_dims(module: torch.nn.Module):
+    #         if hasattr(module, "set_attention_slice"):
+    #             sliceable_head_dims.append(module.sliceable_head_dim)
 
-            for child in module.children():
-                fn_recursive_retrieve_sliceable_dims(child)
+    #         for child in module.children():
+    #             fn_recursive_retrieve_sliceable_dims(child)
 
-        # retrieve number of attention layers
-        for module in self.children():
-            fn_recursive_retrieve_sliceable_dims(module)
+    #     # retrieve number of attention layers
+    #     for module in self.children():
+    #         fn_recursive_retrieve_sliceable_dims(module)
 
-        num_sliceable_layers = len(sliceable_head_dims)
+    #     num_sliceable_layers = len(sliceable_head_dims)
 
-        if slice_size == "auto":
-            # half the attention head size is usually a good trade-off between
-            # speed and memory
-            slice_size = [dim // 2 for dim in sliceable_head_dims]
-        elif slice_size == "max":
-            # make smallest slice possible
-            slice_size = num_sliceable_layers * [1]
+    #     if slice_size == "auto":
+    #         # half the attention head size is usually a good trade-off between
+    #         # speed and memory
+    #         slice_size = [dim // 2 for dim in sliceable_head_dims]
+    #     elif slice_size == "max":
+    #         # make smallest slice possible
+    #         slice_size = num_sliceable_layers * [1]
 
-        slice_size = num_sliceable_layers * [slice_size] if not isinstance(slice_size, list) else slice_size
+    #     slice_size = num_sliceable_layers * [slice_size] if not isinstance(slice_size, list) else slice_size
 
-        if len(slice_size) != len(sliceable_head_dims):
-            raise ValueError(
-                f"You have provided {len(slice_size)}, but {self.config} has {len(sliceable_head_dims)} different"
-                f" attention layers. Make sure to match `len(slice_size)` to be {len(sliceable_head_dims)}."
-            )
+    #     if len(slice_size) != len(sliceable_head_dims):
+    #         raise ValueError(
+    #             f"You have provided {len(slice_size)}, but {self.config} has {len(sliceable_head_dims)} different"
+    #             f" attention layers. Make sure to match `len(slice_size)` to be {len(sliceable_head_dims)}."
+    #         )
 
-        for i in range(len(slice_size)):
-            size = slice_size[i]
-            dim = sliceable_head_dims[i]
-            if size is not None and size > dim:
-                raise ValueError(f"size {size} has to be smaller or equal to {dim}.")
+    #     for i in range(len(slice_size)):
+    #         size = slice_size[i]
+    #         dim = sliceable_head_dims[i]
+    #         if size is not None and size > dim:
+    #             raise ValueError(f"size {size} has to be smaller or equal to {dim}.")
 
-        # Recursively walk through all the children.
-        # Any children which exposes the set_attention_slice method
-        # gets the message
-        def fn_recursive_set_attention_slice(module: torch.nn.Module, slice_size: List[int]):
-            if hasattr(module, "set_attention_slice"):
-                module.set_attention_slice(slice_size.pop())
+    #     # Recursively walk through all the children.
+    #     # Any children which exposes the set_attention_slice method
+    #     # gets the message
+    #     def fn_recursive_set_attention_slice(module: torch.nn.Module, slice_size: List[int]):
+    #         if hasattr(module, "set_attention_slice"):
+    #             module.set_attention_slice(slice_size.pop())
 
-            for child in module.children():
-                fn_recursive_set_attention_slice(child, slice_size)
+    #         for child in module.children():
+    #             fn_recursive_set_attention_slice(child, slice_size)
 
-        reversed_slice_size = list(reversed(slice_size))
-        for module in self.children():
-            fn_recursive_set_attention_slice(module, reversed_slice_size)
+    #     reversed_slice_size = list(reversed(slice_size))
+    #     for module in self.children():
+    #         fn_recursive_set_attention_slice(module, reversed_slice_size)
 
     # def _set_gradient_checkpointing(self, module, value=False):
     #     if isinstance(module, (CrossAttnDownBlock2D, DownBlock2D, CrossAttnUpBlock2D, UpBlock2D)):
