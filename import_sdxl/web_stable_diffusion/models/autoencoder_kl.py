306d80891604c43b1bfbfc7fb4d29c57b9152054
guoyaol <guoyaoli00@gmail.com>
2023-07-26 20:51:52 -0700

    TODO: check one import

--- import_sdxl/web_stable_diffusion/models/autoencoder_kl.py
+++ import_sdxl/web_stable_diffusion/models/autoencoder_kl.py
@@ -18,8 +18,8 @@ import torch
 import torch.nn as nn
 
 # from ..utils import BaseOutput, apply_forward_hook
-#TODO: check these two
-from .attention_processor import AttentionProcessor, AttnProcessor
+# from .attention_processor import AttentionProcessor, AttnProcessor
+#TODO: check this
 from .vae import Decoder, DecoderOutput, DiagonalGaussianDistribution, Encoder
 
 
@@ -155,72 +155,72 @@ class AutoencoderKL():
         """
         self.use_slicing = False
 
-    @property
-    # Copied from diffusers.models.unet_2d_condition.UNet2DConditionModel.attn_processors
-    def attn_processors(self) -> Dict[str, AttentionProcessor]:
-        r"""
-        Returns:
-            `dict` of attention processors: A dictionary containing all attention processors used in the model with
-            indexed by its weight name.
-        """
-        # set recursively
-        processors = {}
+    # @property
+    # # Copied from diffusers.models.unet_2d_condition.UNet2DConditionModel.attn_processors
+    # def attn_processors(self) -> Dict[str, AttentionProcessor]:
+    #     r"""
+    #     Returns:
+    #         `dict` of attention processors: A dictionary containing all attention processors used in the model with
+    #         indexed by its weight name.
+    #     """
+    #     # set recursively
+    #     processors = {}
 
-        def fn_recursive_add_processors(name: str, module: torch.nn.Module, processors: Dict[str, AttentionProcessor]):
-            if hasattr(module, "set_processor"):
-                processors[f"{name}.processor"] = module.processor
+    #     def fn_recursive_add_processors(name: str, module: torch.nn.Module, processors: Dict[str, AttentionProcessor]):
+    #         if hasattr(module, "set_processor"):
+    #             processors[f"{name}.processor"] = module.processor
 
-            for sub_name, child in module.named_children():
-                fn_recursive_add_processors(f"{name}.{sub_name}", child, processors)
+    #         for sub_name, child in module.named_children():
+    #             fn_recursive_add_processors(f"{name}.{sub_name}", child, processors)
 
-            return processors
+    #         return processors
 
-        for name, module in self.named_children():
-            fn_recursive_add_processors(name, module, processors)
+    #     for name, module in self.named_children():
+    #         fn_recursive_add_processors(name, module, processors)
 
-        return processors
+    #     return processors
 
-    # Copied from diffusers.models.unet_2d_condition.UNet2DConditionModel.set_attn_processor
-    def set_attn_processor(self, processor: Union[AttentionProcessor, Dict[str, AttentionProcessor]]):
-        r"""
-        Sets the attention processor to use to compute attention.
+    # # Copied from diffusers.models.unet_2d_condition.UNet2DConditionModel.set_attn_processor
+    # def set_attn_processor(self, processor: Union[AttentionProcessor, Dict[str, AttentionProcessor]]):
+    #     r"""
+    #     Sets the attention processor to use to compute attention.
 
-        Parameters:
-            processor (`dict` of `AttentionProcessor` or only `AttentionProcessor`):
-                The instantiated processor class or a dictionary of processor classes that will be set as the processor
-                for **all** `Attention` layers.
+    #     Parameters:
+    #         processor (`dict` of `AttentionProcessor` or only `AttentionProcessor`):
+    #             The instantiated processor class or a dictionary of processor classes that will be set as the processor
+    #             for **all** `Attention` layers.
 
-                If `processor` is a dict, the key needs to define the path to the corresponding cross attention
-                processor. This is strongly recommended when setting trainable attention processors.
+    #             If `processor` is a dict, the key needs to define the path to the corresponding cross attention
+    #             processor. This is strongly recommended when setting trainable attention processors.
 
-        """
-        count = len(self.attn_processors.keys())
+    #     """
+    #     count = len(self.attn_processors.keys())
 
-        if isinstance(processor, dict) and len(processor) != count:
-            raise ValueError(
-                f"A dict of processors was passed, but the number of processors {len(processor)} does not match the"
-                f" number of attention layers: {count}. Please make sure to pass {count} processor classes."
-            )
+    #     if isinstance(processor, dict) and len(processor) != count:
+    #         raise ValueError(
+    #             f"A dict of processors was passed, but the number of processors {len(processor)} does not match the"
+    #             f" number of attention layers: {count}. Please make sure to pass {count} processor classes."
+    #         )
 
-        def fn_recursive_attn_processor(name: str, module: torch.nn.Module, processor):
-            if hasattr(module, "set_processor"):
-                if not isinstance(processor, dict):
-                    module.set_processor(processor)
-                else:
-                    module.set_processor(processor.pop(f"{name}.processor"))
+    #     def fn_recursive_attn_processor(name: str, module: torch.nn.Module, processor):
+    #         if hasattr(module, "set_processor"):
+    #             if not isinstance(processor, dict):
+    #                 module.set_processor(processor)
+    #             else:
+    #                 module.set_processor(processor.pop(f"{name}.processor"))
 
-            for sub_name, child in module.named_children():
-                fn_recursive_attn_processor(f"{name}.{sub_name}", child, processor)
+    #         for sub_name, child in module.named_children():
+    #             fn_recursive_attn_processor(f"{name}.{sub_name}", child, processor)
 
-        for name, module in self.named_children():
-            fn_recursive_attn_processor(name, module, processor)
+    #     for name, module in self.named_children():
+    #         fn_recursive_attn_processor(name, module, processor)
 
-    # Copied from diffusers.models.unet_2d_condition.UNet2DConditionModel.set_default_attn_processor
-    def set_default_attn_processor(self):
-        """
-        Disables custom attention processors and sets the default attention implementation.
-        """
-        self.set_attn_processor(AttnProcessor())
+    # # Copied from diffusers.models.unet_2d_condition.UNet2DConditionModel.set_default_attn_processor
+    # def set_default_attn_processor(self):
+    #     """
+    #     Disables custom attention processors and sets the default attention implementation.
+    #     """
+    #     self.set_attn_processor(AttnProcessor())
 
     # @apply_forward_hook
     # def encode(self, x: torch.FloatTensor, return_dict: bool = True) -> AutoencoderKLOutput:
