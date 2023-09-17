35800e95caad686dc577f6503bdcb7aa3e913e5f
guoyaol <guoyaoli00@gmail.com>
2023-09-17 16:44:48 -0400

    best check toool

--- import_sdxl/unet_pure_check.py
+++ import_sdxl/unet_pure_check.py
@@ -35,7 +35,7 @@ def wrapper(f, params):
 ########################## Handle Input ##########################
 
 torch.manual_seed(42)
-input1 = torch.rand((1, 4, 128, 128)).to(torch.float32)
+input1 = torch.rand((2, 4, 128, 128)).to(torch.float32)
 input2 = torch.tensor(3).to(torch.int32)
 input3 = torch.rand((2, 77, 2048)).to(torch.float32)
 input4 = torch.rand((2, 1280)).to(torch.float32)
@@ -50,39 +50,21 @@ input5_nd = tvm.nd.array(input5, device=device)
 
 
 ########################## Reference Part ##########################
-class UNetModelWrapper(torch.nn.Module):
-    def __init__(self, unet):
-        super().__init__()
-        self.unet = unet
-        # Default guidance scale factor in stable diffusion.
-        self.guidance_scale = 5.0
-
-    def forward(self, latents, timestep_tensor, text_embeddings, added_cond_kwargs_text_embeds, added_cond_kwargs_text_time_ids):
-        # Latent concatenation.
-        latent_model_input = torch.cat([latents] * 2, dim=0)
-        # UNet forward.
-        noise_pred = self.unet(latent_model_input, timestep_tensor, text_embeddings, added_cond_kwargs_text_embeds, added_cond_kwargs_text_time_ids)
-        # Classifier-free guidance.
-        noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-        noise_pred = noise_pred_uncond + self.guidance_scale * (
-            noise_pred_text - noise_pred_uncond
-        )
-        return noise_pred
 
 device_str = "cpu"
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0")
 
 ref_unet = utils.get_unet(pipe, device_str)
 ref_unet.eval()
-unet_to_noise_pred = UNetModelWrapper(ref_unet)
+# unet_to_noise_pred = UNetModelWrapper(ref_unet)
 
-unet_to_noise_pred.eval()
+# unet_to_noise_pred.eval()
 with torch.no_grad():
-    ref_result = unet_to_noise_pred(input1, input2, input3, input4, input5)
+    ref_result = ref_unet(input1, input2, input3, input4, input5)
 
 ref_result = ref_result.numpy()
 print("ref result")
-print(ref_result)
+# print(ref_result)
 
 
 ########################## Our Part ##########################
@@ -90,9 +72,28 @@ print("our result")
 
 def unet_latents_to_noise_pred(pipe, device_str: str) -> tvm.IRModule:
 
+    class UNetModelWrapper(torch.nn.Module):
+        def __init__(self, unet):
+            super().__init__()
+            self.unet = unet
+            # Default guidance scale factor in stable diffusion.
+            self.guidance_scale = 5.0
+
+        def forward(self, latents, timestep_tensor, text_embeddings, added_cond_kwargs_text_embeds, added_cond_kwargs_text_time_ids):
+            # # Latent concatenation.
+            # latent_model_input = torch.cat([latents] * 2, dim=0)
+            # # UNet forward.
+            noise_pred = self.unet(latents, timestep_tensor, text_embeddings, added_cond_kwargs_text_embeds, added_cond_kwargs_text_time_ids)
+            # # Classifier-free guidance.
+            # noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            # noise_pred = noise_pred_uncond + self.guidance_scale * (
+            #     noise_pred_text - noise_pred_uncond
+            # )
+            return noise_pred
+
     unet = utils.get_unet(pipe, device_str)
-    # unet_to_noise_pred = UNetModelWrapper(unet)
-    graph = fx.symbolic_trace(unet)
+    unet_to_noise_pred = UNetModelWrapper(unet)
+    graph = fx.symbolic_trace(unet_to_noise_pred)
     mod = from_fx(
         graph,
         [((2, 4, 128, 128), "float32"), ((), "int32"), ((2, 77, 2048), "float32"), 
@@ -157,8 +158,8 @@ nd_res1 = imported_unet(input1_nd, input2_nd, input3_nd, input4_nd, input5_nd)
 our_out = nd_res1.numpy()
 
 
-print(our_out)
+# print(our_out)
 
 
 import numpy as np
-np.testing.assert_allclose(our_out, ref_result, atol=1e-2, rtol=0.1)
+np.testing.assert_allclose(our_out, ref_result)
