6ab1d60a5c28209a21ac4f862c72e476cfab11af
guoyaol <guoyaoli00@gmail.com>
2023-10-10 20:56:12 -0400

    update

--- import_sdxl/iso_getitem.py
+++ import_sdxl/iso_getitem.py
@@ -2,19 +2,26 @@ from torch import nn
 import torch
 from tvm.relax.frontend.torch import dynamo_capture_subgraphs
 
+torch.manual_seed(42)
+
 class my_model(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, last_hidden_state):
-        index2 = last_hidden_state.argmax(dim=-1)
-        print("index2")
-        print(index2)
-        pooled_output = last_hidden_state[[0],last_hidden_state.argmax(dim=-1)]
+    def forward(self, last_hidden_state, input_ids):
+
+        index1 = torch.arange(last_hidden_state.shape[0])
+        index2 = input_ids.argmax(dim=-1)
+
+        # pooled_output = last_hidden_state[index1, index2, ]
+        # squeezed_hidden_states = last_hidden_state.squeeze(0)
+        pooled_output = last_hidden_state[torch.arange(1), ]
+
         return pooled_output
 
 
-text_input_ids = torch.rand((1, 77))
+text_input_ids = torch.rand((1, 77, 1280))
+random_ids = torch.rand((1, 77))
 # print(text_input_ids)
 # Capture CLIP's computational graph.
 
@@ -22,11 +29,14 @@ model = my_model()
 
 
 
-# output = model(text_input_ids)
-# print(output)
+output = model(text_input_ids, random_ids)
+print("model output", output.shape)
 
 mod = dynamo_capture_subgraphs(
     model.forward,
     text_input_ids,
+    random_ids,
     keep_params_as_input=True,
 )
+
+print(mod)
