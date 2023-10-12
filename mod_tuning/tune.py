8727d1c8de0c53e4dfb36b9fe4d73253220f75cc
guoyaol <guoyaoli00@gmail.com>
2023-10-11 21:46:23 -0400

    add tune script

---
+++ mod_tuning/tune.py
@@ -0,0 +1,25 @@
+def tune(mod: tvm.IRModule) -> None:
+    from tvm import meta_schedule as ms
+
+    ms.relax_integration.tune_relax(
+        mod=mod,
+        target=tvm.target.Target("apple/m1-gpu-restricted"),
+        params={},
+        builder=ms.builder.LocalBuilder(
+            max_workers=6,
+        ),
+        runner=ms.runner.RPCRunner(
+            ms.runner.RPCConfig(
+                tracker_host="192.168.10.1",
+                tracker_port=9191,
+                tracker_key="m2-mac-mini",
+                session_timeout_sec=50,
+            )
+        ),
+        work_dir="log_db_tuning",
+        max_trials_global=50000,
+        max_trials_per_task=2000,
+        strategy=ms.search_strategy.EvolutionarySearch(init_min_unmeasured=10, max_fail_count=20),
+    )
+
+tune(mod_deploy)
